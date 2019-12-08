8523d9d4e473de11f07c75eaad43492a8a2928bb
Joshua <joshua@aleph-labs.com>
2019-12-09 00:05:58 +0700

    act 1

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListItems from './ListItems'
+import Firebase from './Firebase/config'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+
+library.add(faTrash)
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    console.log('DATA SAVED');
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key){
+   
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text,key){
+    
+    console.log("items:"+this.state.items);
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+    
+   
+  }
+
+  writeUserData = () => {
+    Firebase.database().ref('/').set(this.state);
+    console.log('DATA SAVED');
+  }
+  
+
+ render(){
+  return (
+    <div className="App">
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="masukin kerjaan.." value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type="submit">tambahin</button>
+        </form>
+        <p>{this.state.items.text}</p>
+        
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+        
+      </header>
+    </div>
+  );
+ }
+}
+
+
+export default App;
---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyB8dVApULvVEavnM913d8yaWEVv3CINYrY",
+    authDomain: "firetodo-13ec8.firebaseapp.com",
+    databaseURL: "https://firetodo-13ec8.firebaseio.com",
+    projectId: "firetodo-13ec8",
+    storageBucket: "firetodo-13ec8.appspot.com",
+    messagingSenderId: "646699508695",
+    appId: "1:646699508695:web:299a40baf8956ce5fc2192",
+    measurementId: "G-3NSKD8S5W4"
+  };
+
+  export default config;
